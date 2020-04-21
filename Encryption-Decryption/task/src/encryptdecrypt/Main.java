a66496a11813d6188cfca45e43c3f3f52b36287e
dark-crusader <shubham.rubal31@gmail.com>
2020-04-21 20:22:00 +0530

    Changed encryption. Added decryption support.

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -3,27 +3,39 @@ package encryptdecrypt;
 import java.util.Scanner;
 
 public class Main {
+    @Deprecated
     private static boolean isAlpha(char c) {
         return c >= 'a' && c <= 'z';
     }
 
-    private static String encrypt(String text, int key) {
+    private String encrypt(String text, int key) {
         char[] input = text.toCharArray();
         char[] res = new char[text.length()];
         for(int i=0; i<text.length(); i++) {
-            if(isAlpha(input[i])) {
-                res[i] = (char) (((input[i] - 'a' + key) % 26) + 'a');
-            } else {
-                res[i] = input[i];
-            }
+
+                res[i] = (char)(input[i] + key);
+
         }
         return String.valueOf(res);
     }
+
+
+    public String operate(String op, String text, int key) {
+        if(op.equals("enc")) {
+            return encrypt(text, key);
+        } else if(op.equals("dec")) {
+            return encrypt(text, -key);
+        } else {
+            return "Please provide a valid operation input.";
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String op = scanner.nextLine();
         String text = scanner.nextLine();
         int key = scanner.nextInt();
-        System.out.println(encrypt(text, key));
+        System.out.println(new Main().operate(op, text, key));
 
     }
 }
