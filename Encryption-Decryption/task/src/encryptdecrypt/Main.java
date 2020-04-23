6b123e1f1da40ffba2c81a04a8e6b25b036f0990
dark-crusader <shubham.rubal31@gmail.com>
2020-04-23 15:36:29 +0530

    Added command line support and comments.

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,14 +1,15 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
 
 public class Main {
+    // Auxiliary to check for alphabetic input
     @Deprecated
     private static boolean isAlpha(char c) {
         return c >= 'a' && c <= 'z';
     }
 
-    private String encrypt(String text, int key) {
+    //Function used for rolling encryption and decryption
+    private static String encrypt(String text, int key) {
         char[] input = text.toCharArray();
         char[] res = new char[text.length()];
         for(int i=0; i<text.length(); i++) {
@@ -19,24 +20,34 @@ public class Main {
         return String.valueOf(res);
     }
 
-
-    public String operate(String op, String text, int key) {
-        
-        if(op.equals("enc")) {
-            return encrypt(text, key);
-        } else if(op.equals("dec")) {
-            return encrypt(text, -key);
-        } else {
-            return "Please provide a valid operation input.";
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String op = scanner.nextLine();
-        String text = scanner.nextLine();
-        int key = scanner.nextInt();
+
+        // Default arguments provided.
+        String op = "enc";
+        int key = 0;
+        String text = "";
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-mode")) {
+                op = args[i + 1];
+            } else if (args[i].equals("-key")) {
+                key = Integer.parseInt(args[i + 1]);
+            } else if (args[i].equals("-data")) {
+                text = args[i + 1];
+            }
+        }
+
         System.out.println(new Main().operate(op, text, key));
 
+    }
+
+    //Interface to call function
+    public String operate(String op, String text, int key) {
+        if (op.equals("enc")) {
+            return encrypt(text, key);
+        } else if (op.equals("dec")) {
+            return encrypt(text, -key);
+        } else {
+            return "Unknown operation";
+        }
     }
 }
